4afc6efde172ea6f78a3de38aac25a5de8201864
Antti Nurmi <antti.nurmi@tuni.fi>
2026-09-14 10:21:43 +0300

    Add interface ports and apb tieoff

--- rtl/apb_timer_queue.sv
+++ rtl/apb_timer_queue.sv
@@ -1,6 +1,15 @@
 module apb_timer_queue #(
 ) (
-    input logic clk_i,
-    input logic rst_ni
+    input  logic            clk_i,
+    input  logic            rst_ni,
+    output logic            irq_pl_o,
+    output logic            irq_full_o,
+    output logic            irq_nfull_o,
+    input  logic     [63:0] mtime_i,
+           APB.Slave        apb_sbr
 );
+
+  assign apb_sbr.pslverr   = 1'b0;
+  assign apb_sbr.pready = apb_sbr.psel & apb_sbr.penable;
+
 endmodule : apb_timer_queue
