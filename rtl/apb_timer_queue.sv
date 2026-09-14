a5549b50a488caa856e63cbf1c0ebaa4df5aa2ae
Antti Nurmi <antti.nurmi@tuni.fi>
2026-09-14 14:30:47 +0300

    Correct interface and parameters

--- rtl/apb_timer_queue.sv
+++ rtl/apb_timer_queue.sv
@@ -1,12 +1,15 @@
 module apb_timer_queue #(
+    parameter  int unsigned Depth    = 8,
+    parameter  int unsigned NrIrqs   = 32,
+    localparam int unsigned IrqWidth = $clog2(NrIrqs)
 ) (
-    input  logic            clk_i,
-    input  logic            rst_ni,
-    output logic            irq_pl_o,
-    output logic            irq_full_o,
-    output logic            irq_nfull_o,
-    input  logic     [63:0] mtime_i,
-           APB.Slave        apb_sbr
+    input  logic                  clk_i,
+    input  logic                  rst_ni,
+    output logic     [NrIrqs-1:0] irq_pl_o,
+    output logic                  irq_full_o,
+    output logic                  irq_nfull_o,
+    input  logic     [      63:0] mtime_i,
+           APB.Slave              apb_sbr
 );
 
   assign apb_sbr.prdata  = 32'b0;
